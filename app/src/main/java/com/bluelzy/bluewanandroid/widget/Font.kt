b56640f17a0bfb58fe1ff0d863948ff04be2b594
bluelzy <bluehobert@gmail.com>
2020-05-30 00:09:32 +0800

    Add Different Fonts.

---
+++ app/src/main/java/com/bluelzy/bluewanandroid/widget/Font.kt
@@ -0,0 +1,16 @@
+package com.bluelzy.bluewanandroid.widget
+
+/**
+ *   @author    BlueLzy
+ *   @email     bluehobert@gmail.com
+ *   @date      2020/5/29
+ *   @desc      Fonts Collection
+ */
+interface Font {
+
+    companion object {
+        var LARSSEIT_LIGHT: String = "fonts/Type Dynamic - Larsseit Light.otf"
+
+        var LARSSEIT_MEDIUM = "fonts/Type Dynamic - Larsseit Medium.otf"
+    }
+}
---
+++ app/src/main/java/com/bluelzy/bluewanandroid/widget/LarsseitLightTextView.kt
@@ -0,0 +1,24 @@
+package com.bluelzy.bluewanandroid.widget
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import com.bluelzy.bluewanandroid.widget.Font.Companion.LARSSEIT_LIGHT
+
+/**
+ *   @author    BlueLzy
+ *   @email     bluehobert@gmail.com
+ *   @date      2020/5/29
+ *   @desc
+ */
+class LarsseitLightTextView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    init {
+        if (!isInEditMode) {
+            typeface = Typeface.createFromAsset(getContext().assets, LARSSEIT_LIGHT)
+        }
+    }
+}
